5b12021013b060cee2cb7e1972a469aafb98bb85
ermes-x <eglis.mella@ermes-x.com>
2021-02-21 20:35:38 +0100

    test background

--- android/common/src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
+++ android/common/src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
@@ -52,10 +52,12 @@ public class BackgroundGeolocationFacade {
     public static final int AUTHORIZATION_AUTHORIZED = 1;
     public static final int AUTHORIZATION_DENIED = 0;
 
-    public static final String[] PERMISSIONS = {
+    public static final String[] PERMISSIONS10 = {
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
     };
+    private String[] PERMISSIONSNEW = PERMISSIONS;
 
     private boolean mServiceBroadcastReceiverRegistered = false;
     private boolean mLocationModeChangeReceiverRegistered = false;
@@ -212,10 +214,14 @@ public class BackgroundGeolocationFacade {
     }
 
     public void start() {
-        logger.debug("Starting service");
-
+        if(android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            PERMISSIONSNEW = PERMISSIONS;
+        }else {
+            PERMISSIONSNEW = PERMISSIONS10;
+        }
+            logger.debug("Starting service");
         PermissionManager permissionManager = PermissionManager.getInstance(getContext());
-        permissionManager.checkPermissions(Arrays.asList(PERMISSIONS), new PermissionManager.PermissionRequestListener() {
+        permissionManager.checkPermissions(Arrays.asList(PERMISSIONSNEW), new PermissionManager.PermissionRequestListener() {
             @Override
             public void onPermissionGranted() {
                 logger.info("User granted requested permissions");
